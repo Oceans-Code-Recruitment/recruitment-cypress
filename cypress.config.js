5c96d56660b9d21d7b4caf3223bdf17d339b3ec1
Jose Cabrera <joseignaciocabrerabustamante@gmail.com>
2022-06-10 17:40:07 -0400

    Updated Cypress Version

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   // These settings apply everywhere unless overridden
   defaultCommandTimeout: 1000,
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
